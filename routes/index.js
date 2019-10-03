f617e2f86b2865eba72ea97d3babf309e47098c9
HawkKim <dummy@gmail.com>
2019-10-04 00:00:48 +0900

    딕셔너리 디비에 추가 페이지 작성, response 는 단순 출력만

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OPR Helper For IRK Agents' });
 });
 
 module.exports = router;
