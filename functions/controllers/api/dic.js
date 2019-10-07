06a466697715aaa0bbc383e6a22da46bbc7a31cd
HawkBro <kimaeonhawk@gmail.com>
2019-10-07 23:40:10 +0900

    #7 수정 request 와 받아주는 controller 코드 작성

--- functions/controllers/api/dic.js
+++ functions/controllers/api/dic.js
@@ -13,15 +13,15 @@ router.post('/dic', async (req, res, next) => {
   /**
    * @type { string } 한글
    */
-  var korean = req.body['korean'];
+  var korean = req.body.korean;
   /**
    * @type { string } 번역전 원문
    */
-  var english = req.body['english'].toLowerCase();
+  var english = req.body.english.toLowerCase();
   /**
    * @type { string } 등록자 코드네임
    */
-  var codename = req.body['codename'];
+  var codename = req.body.codename;
 
   /**
    * @type { ApiResponse }
@@ -30,7 +30,32 @@ router.post('/dic', async (req, res, next) => {
   res.json(responseBody);
 });
 
+/**
+ * 사전 수정 컨트롤러
+ */
+router.put('/dic', async (req, res, next) => {
+  var key = req.body.key;
+  var english = req.body.english;
+  var korean = req.body.korean;
+
+  if (
+    (english == undefined || english == '') &&
+    (korean == undefined || korean == '')
+  )
+    return res.json(new ApiResponse(false, 'VALUE IS EMPTY', null));
+  if (key == undefined || key == '')
+    return res.json(new ApiResponse(false, 'KEY IS EMPTY', null));
+
+  return res.json(new ApiResponse(true, null, null));
+});
+
+/**
+ * 사전 삭제 컨트롤러
+ */
 router.delete('/dic', async (req, res, next) => {
+  /**
+   * @type { string } Key
+   */
   var key = req.body.key;
   if (key == undefined || key == '')
     return res.json(new ApiResponse(false, 'KEY IS EMPTY', null));
