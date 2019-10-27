1f6db68a761072c0272df369e41090a97972e8dd
HawkBro <kimaeonhawk@gmail.com>
2019-10-28 01:13:59 +0900

    #42 제품키 등록하는 api 추가

--- functions/controllers/api/api.key.js
+++ functions/controllers/api/api.key.js
@@ -8,29 +8,54 @@ const model = require('../../models/model.user');
 
 // #region Public functions
 
+router.use(async (req, res, next) => {
+  var key = req.query.key ? req.query.key : req.body.key;
+  var user = req.user;
+
+  if (user == null) res.status(403).send();
+  if (key == null) res.json(new ApiResponse(false, 'NO KEY', null));
+
+  req.user = user;
+  next();
+});
+
+/**
+ * 제품키 유효성 검사
+ * 유효기간은 검사하지 않음
+ */
 router.get('/key/verify', async (req, res, next) => {
   var key = req.query.key;
   var user = req.user;
 
-  if (user == null) res.status(403).send();
-  if (key == null) res.json(new apifunc(false, 'NO KEY', null));
-
-  var result = await model.keyVerify(key, user.email);
-
-  res.json(new apifunc(result != null && result.length > 0, null, null));
+  try {
+    var result = await model.keyVerify(key, user.email);
+    res.json(new ApiResponse(result != null && result.length > 0, null, null));
+  } catch (err) {
+    console.log('err');
+    res.json(new ApiResponse(false, null, err));
+  }
 });
 
+/**
+ * 제품키 등록
+ */
 router.post('/key/activate', async (req, res, next) => {
-  var key = req.query.key;
+  var key = req.body.key;
   var user = req.user;
 
-  if (user == null) res.status(403).send();
-  if (key == null) res.json(new apifunc(false, 'NO KEY', null));
-
+  // 유효성 검사 먼저
   var result = await model.keyVerify(key, user.email);
 
   if (result != null && result.length > 0 == false)
     return res.status(403).send();
+
+  try {
+    await model.keyActivate(key, user.email, user.uid);
+    res.json(new ApiResponse(true, null, null));
+  } catch (err) {
+    console.log(err);
+    res.json(new ApiResponse(false, null, err));
+  }
 });
 
 // #endregion
--- functions/models/model.user.js
+++ functions/models/model.user.js
@@ -14,126 +14,51 @@ var functions = require('./class/functions');
  */
 async function keyVerify(key, email) {
   try {
-    const searched = ref.orderByChild('key').equalTo(key);
+    const searched = ref.orderByChild('productKey').equalTo(key);
     var fetched = await searched.once('value');
     fetched = fetched.val();
+
     if (fetched == null) return null;
 
-    result = result.filter((v, i, a) => {
+    var result = fetched.filter((v, i, a) => {
       return v.email == email && v.isActive == false;
     });
-
-    return result;
-  } catch (err) {
-    return false;
-  }
-}
-module.exports.keyVerify = keyVerify;
-
-async function search(name, x, y, codename) {
-  try {
-    const searched = await ref.orderByChild('name').equalTo(name);
-    var fetched = await searched.once('value');
-    fetched = fetched.val();
-
-    if (fetched == null) return null;
-
-    // 연관 오브젝트를 배열로 변환, 키값은 property 로 변환
-    var result = [];
-    Object.keys(fetched).map((key, i, a) => {
-      result.push(fetched[key]);
-    });
-
-    // GPS 좌표로 필터링
-    result = result.filter((v, i, a) => {
-      return v.x == x && v.y == y;
-    });
-
-    // 코드네임이 있으면 코드네임도 필터링
-    if (isEmpty(codename) === false)
-      result = result.filter((v, i, a) => {
-        return v.codenameLower == codename.toLowerCase();
-      });
 
     return result;
   } catch (err) {
     throw err;
   }
 }
-module.exports.search = search;
+module.exports.keyVerify = keyVerify;
 
-/**
- * 새 Entity를 등록
- * @returns { ApiResponse }
- */
-async function add(name, x, y, image, codename) {
-  var newRow = {
-    name: name,
-    x: x,
-    y: y,
-    image: image,
-    creator: codename,
-    updater: codename,
-    codenameLower: codename.toLowerCase(),
-    createtime: moment()
-      .tz('Asia/Seoul')
-      .format('YYYY-MM-DD HH:mm'),
-    updatetime: moment()
-      .tz('Asia/Seoul')
-      .format('YYYY-MM-DD HH:mm'),
-  };
+module.exports.keyActivate = async (key, email, uid) => {
   try {
-    // set() 은 return 값이 error
-    var newDocRef = ref.push();
-    const err = await newDocRef.set(newRow);
-    newRow.key = newDocRef.key;
+    var verify = await keyVerify(key, email);
+    if (verify == null) return null;
 
-    if (err) return new ApiResponse(false, err, null);
-    else return new ApiResponse(true, null, newRow);
+    var val = await ref
+      .orderByChild('productKey')
+      .equalTo(key)
+      .once('value');
+
+    val = functions.objToArray(val.val());
+    console.log(val);
+
+    const doc = ref.child(val[0].key);
+
+    var newdoc = {
+      isActive: true,
+      uid: uid,
+      updatetime: moment()
+        .tz('Asia/Seoul')
+        .format('YYYY-MM-DD HH:mm'),
+    };
+
+    await doc.update(newdoc);
+    return new ApiResponse(true, null, doc);
   } catch (err) {
-    return new ApiResponse(false, err, null);
+    throw err;
   }
-}
-module.exports.add = add;
-
-/*async function test() {
-  try {
-    const searched = await ref.orderByChild('name');
-    var fetched = await searched.once('value');
-    fetched = fetched.val();
-
-    if (fetched == null) return null;
-
-    Object.keys(fetched).map(async (key, i, a) => {
-      console.log(fetched[key].creator);
-
-      var trimed = fetched[key].creator.trim();
-
-      const doc = ref.child(key);
-      var newdoc = {
-        creator: trimed,
-        updater: fetched[key].updater.trim(),
-        codenameLower: trimed.toLowerCase(),
-      };
-
-      await doc.update(newdoc);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-module.exports.test = test;*/
-
-// #endregion
-
-// #region Private funcs
-
-/**
- * 비었거나 null 인지 체크
- * @param { string } value 체크 할 값
- */
-function isEmpty(value) {
-  return value == undefined || value == '';
-}
+};
 
 // #endregion
